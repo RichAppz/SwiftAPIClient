445f610fbdde0fbe1e3518e9797dd383aa99db18
Rich Mucha <rich@richappz.com>
2021-12-28 11:01:14 +0000

    updated package

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-//        .binaryTarget(
-//            name: "SwiftAPIClient",
-//            path: "./output/SwiftAPIClient.xcframework"
-//        ),
         .binaryTarget(
             name: "SwiftAPIClient",
-            url: "https://github.com/RichAppz/SwiftAPIClient/blob/master/SwiftAPIClient.xcframework.zip",
-            checksum: "788d0f1095344f8c135e47d0faab6cd90f24bda3b7812a2378aa0da00ee8f918"
-        )
+            path: "./output/SwiftAPIClient.xcframework"
+        ),
+//        .binaryTarget(
+//            name: "SwiftAPIClient",
+//            url: "https://github.com/RichAppz/SwiftAPIClient/blob/master/SwiftAPIClient.xcframework.zip",
+//            checksum: "788d0f1095344f8c135e47d0faab6cd90f24bda3b7812a2378aa0da00ee8f918"
+//        )
     ]
 )
