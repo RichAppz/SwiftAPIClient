bf4fe0d60b81c7adc4584f326428fabc5d2566a3
Rich Mucha <rich@richappz.com>
2024-05-20 22:02:48 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftAPIClient",
-            exclude: ["SwiftAPIClient.podspec"],
             dependencies: []),
         .testTarget(
             name: "SwiftAPIClient-Tests",
