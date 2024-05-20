d4b42b4c6d645f2a477571cd5efcb0b8f7b0b1c3
Rich Mucha <rich@richappz.com>
2024-05-20 21:37:36 +0100

    SwiftPackage

---
+++ Package.swift
@@ -0,0 +1,39 @@
+//
+//  Package.swift
+//  SwiftAPIClient
+//
+//  Created by Rich Mucha on 20/05/2024.
+//  Copyright © 2024 RichAppz Limited. All rights reserved.
+//
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SwiftAPIPackage",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13), .watchOS(v10)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SwiftAPIPackage",
+            targets: ["SwiftAPIPackage"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "SwiftAPIPackage",
+            exclude: ["SwiftAPIClient.podspec"],
+            dependencies: []
+        ),
+        .testTarget(
+            name: "SwiftAPIClient-Tests",
+            dependencies: ["SwiftAPIPackage"]
+        )
+    ]
+)
